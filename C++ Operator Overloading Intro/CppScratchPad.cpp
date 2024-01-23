ab42e722db5ef2adc789fb2ac270bbb96c41f936
Kulpot <kulpotwahu@gmail.com>
2024-01-23 17:06:51 +0800

    ...

--- C++ Operator Overloading Intro/CppScratchPad.cpp
+++ C++ Operator Overloading Intro/CppScratchPad.cpp
@@ -5,7 +5,7 @@ using std::endl;
 // ---------------------- C++ Operator Overloading Intro -------------------------------
 //ref link:https://www.youtube.com/watch?v=IfdakFjYxEg&list=PLRwVmtr-pp077ESC0tAC331LxLaDa5A57
 
-struct Vector
+struct Vector		// algebra/trigonometry knowledge required
 {
 	int x;
 	int y;
@@ -17,6 +17,10 @@ void main()
 	v1.x = 2; v1.y = 3;
 	Vector v2;
 	v2.x = 1; v2.y = 4;
+	Vector result;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	cout << result.x << " " << result.y << endl;
 }
 
 //int add(int left, int right)
