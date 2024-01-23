6b56966ff703bd9b9abe4e07fce90a7e44f2e740
Kulpot <kulpotwahu@gmail.com>
2024-01-23 16:52:01 +0800

    ...

---
+++ C++ Operator Overloading Intro/CppScratchPad.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+using std::end;
+
+// ---------------------- C++ Operator Overloading Intro -------------------------------
+//ref link:https://www.youtube.com/watch?v=IfdakFjYxEg&list=PLRwVmtr-pp077ESC0tAC331LxLaDa5A57
+
+void main()
+{
+
+}
