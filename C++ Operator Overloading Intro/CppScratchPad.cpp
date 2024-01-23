8c85317d6393b2617406eaf1f95959f9a2717055
Kulpot <kulpotwahu@gmail.com>
2024-01-23 16:56:07 +0800

    ...

--- C++ Operator Overloading Intro/CppScratchPad.cpp
+++ C++ Operator Overloading Intro/CppScratchPad.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using std::cout;
-using std::end;
+using std::endl;
 
 // ---------------------- C++ Operator Overloading Intro -------------------------------
 //ref link:https://www.youtube.com/watch?v=IfdakFjYxEg&list=PLRwVmtr-pp077ESC0tAC331LxLaDa5A57
 
+int add(int left, int right)
+{
+	return;
+}
+
 void main()
 {
-
+	int first = 43;
+	int second = 4398;
+	int result = first + second;
+	int result2 = add(first, second);
+	cout << result << endl;
+	cout << result2 << endl;
 }
